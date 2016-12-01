b0ce9dda578a1562f6c9271ee2fbf1be18dd059a
JergenJones <jergenjones@gmail.com>
2016-12-01 16:57:46 -0600

    12-6 use a single post subscription to ensure tha twe can always see the right post

--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,13 @@
 // This .publish method has a more secure pattern, detailed in ch 12: pagination
-// It works as is for our little app
+// A later example changed this again, so I'm a bit lost on what the other
+// pattern was about
 Meteor.publish('posts', function(options) {
-  check(options, {
-    sort: Object,
-    limit: Number
-  });
   return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function(id) {
+  check(id, String)
+  return Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId) {
